3c7c34ab7cb6df001e12d6bfc7e2f673fe307d42
nacht29 <nacht29.study@gmail.com>
2025-06-18 14:16:59 +0800

    feat: added json files to set up workspace and use venv as python interpreter

--- myvenv/bin/activate.fish
+++ myvenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /mnt/c/Users/user/Documents/calendar-reminder-bot/myvenv
+set -gx VIRTUAL_ENV /mnt/c/Users/Asus/Desktop/calendar-reminder-bot/myvenv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
